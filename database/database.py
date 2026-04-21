c0add5eb6be7f5da54272dfe70f77b51342ceea9
xongs <xongsmint@gmail.com>
2026-04-20 21:53:58 -0300

    database conn

--- database/database.py
+++ database/database.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///sqlalchemy_example.db')
 Base = declarative_base()
