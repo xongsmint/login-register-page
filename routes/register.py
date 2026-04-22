d91433fc800916ce19f6e863a359334d8a7dbef4
xongs <xongsmint@gmail.com>
2026-04-22 04:28:55 -0300

    added jwt for registering

--- routes/register.py
+++ routes/register.py
@@ -1,4 +1,27 @@
 from flask import Blueprint, request
-from json import loads
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
+from repositories.user_repo import UserRepo
+from database.database import SessionLocal
 
 register_bp = Blueprint("register", __name__)
+
+@register_bp.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+    nickname = data.get("nickname")
+
+    if not all([email, password, nickname]):
+        return { "error": "400 bad request" }
+
+    session = SessionLocal()
+    repo = UserRepo(session=session)
+
+    try:
+        user = repo.register_user(email=email, password=password, nickname=nickname)
+        accessToken = create_access_token(identity=user["id"])
+        return { "user": user, "accessToken": accessToken }
+    except Exception as e:
+        return { "message": f"{str(e)}", "error": "lowkey internal server error 500" }
